e219ec050942842706209d4b2330d126e45e243a
balbant <balbant61201@gmail.com>
2025-02-09 20:34:29 +0530

    final commit in js

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
-    
-        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-        if (response.status == 404) {
-            document.querySelector(".error").style.display = "block";
-            document.querySelector(".weather").style.display = "none";
-        } else{
-            let data = await response.json();
+
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        let data = await response.json();
         console.log(data);
 
         document.querySelector(".city").innerHTML = data.name;
@@ -32,14 +32,14 @@ async function checkWeather(city) {
             weatherIcon.src = "images/mist.png";
         } else if (data.weather[0].main == "Snow") {
             weatherIcon.src = "images/snow.png";
-        } else  {
+        } else {
             weatherIcon.src = "images/clear.png";
         }
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
 
-        }
-        
+    }
+
 }
 
 searchBtn.addEventListener("click", () => {
